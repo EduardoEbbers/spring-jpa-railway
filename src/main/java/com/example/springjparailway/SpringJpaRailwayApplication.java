6f18cd74bc9f757c60b378eb3f5911d1c9da7e2e
duda <duda_ebbers@hotmail.com>
2023-08-31 02:19:14 -0300

    projeto spring

---
+++ src/main/java/com/example/springjparailway/SpringJpaRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjparailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjparailway/SpringJpaRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjparailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
